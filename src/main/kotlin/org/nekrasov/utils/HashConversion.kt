c3bcba12ac243b5cb712b8ac1c87a64b65d58567
VladNekrasov <vlad.nekrasov.555@inbox.ru>
2024-02-01 08:05:34 +0300

    Rewrite HashConversion

--- src/main/kotlin/org/nekrasov/utils/HashConversion.kt
+++ src/main/kotlin/org/nekrasov/utils/HashConversion.kt
@@ -1,8 +1,10 @@
 package org.nekrasov.utils
 import org.mindrot.jbcrypt.BCrypt
+
+
 fun hashPassword(password: String): String {
-    return BCrypt.hashpw(password, BCrypt.gensalt())
+    return BCrypt.hashpw(password, BCrypt.gensalt(12)).substring(7)
 }
 fun checkPassword(password: String, hashedPassword: String): Boolean {
-    return BCrypt.checkpw(password, hashedPassword)
+    return BCrypt.checkpw(password, "$2a$12$$hashedPassword")
 }
